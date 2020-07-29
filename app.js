d79eb0efad7a3558098eedfdabe63273d7ed1fc7
dowap2 <dowapdowari@gmail.com>
2020-07-29 21:12:21 +0900

    modify code

--- app.js
+++ app.js
@@ -11,65 +11,81 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.connect('mongodb://localhost/board', {useNewUrlParser: true});
 var db = mongoose.connection;
 
-db.once('open', function(){
+db.once('open', function() {
   console.log('DB connected');
 });
-db.on('error', function(err){
+
+db.on('error', function(err) {
   console.log('DB ERROR : ', err);
 });
 
-app.use(cors())
+app.use(cors());
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-var boardSchema = mongoose.Schema({
-    title:{type:String, required:true, unique:true},
+var boardSchema = mongoose.Schema (
+  {title:{type:String, required:true, unique:true},
     main:{type:String},
     time:{type:String},
     like:{type:Number},
-    bad:{type:Number},
-  });
-  var Contact = mongoose.model('contacts', boardSchema);
+    bad:{type:Number}}
+);
 
-  app.get('/api', function(req, res){
-    Contact.find({}, function(err, contacts){
-      if(err) return res.json(err);
+var Contact = mongoose.model('contacts', boardSchema);
+
+app.get('/api', function(req, res) {
+  Contact.find({}, function(err, contacts) {
+      if(err) {
+        return res.json(err);
+      }
+      //
       res.send({contact: contacts});
   });
+});
 
-  });
-
-  app.post('/api', function(req, res){
-    Contact.create(req.body, function(err, contact){
-      if(err) return console.log(err);
+app.post('/api', function(req, res) {
+  Contact.create(req.body, function(err, contact) {
+    if(err) {
+      return console.log(err);
+    } else {
       console.log(req.body)
+    }
   });
+});
 
-  app.put('/api' , function(req, res){
-    console.log(req)
-    Contact.update({_id: req.body.params._id} , {$set: {title: req.body.data.title , main: req.body.data.main}}, function(err){
-      if(err) return console.log("error");
+app.put('/api' , function(req, res) {
+  Contact.update({_id: req.body.params._id} , {$set: {title: req.body.data.title , main: req.body.data.main}}, function(err){
+    if(err) {
+      return console.log("error");
+    } else {
       console.log(req)
-    })
-  })
-  app.put('/api/like' , function(req, res){
-    console.log(req)
-    Contact.update({_id: req.body.params._id} , {$set: {like: req.body.data.like , bad: req.body.data.bad}}, function(err){
-      if(err) return console.log(err);
-      console.log(req)
-    })
-  })
+    }
+  });
+});
 
-  app.delete('/api', function(req, res){
-    Contact.deleteOne({_id: req.body.id}, function(err){
-      if(err) return console.log("error");
+app.delete('/api', function(req, res) {
+  Contact.deleteOne({_id: req.body.id}, function(err) {
+    if(err) {
+      return console.log("error");
+    } else {
       console.log(req)
-    });
+    }
   });
-  });
+});
+
+app.put('/api/like' , function(req, res) {
+  Contact.update({_id: req.body.params._id}, {$set: {like: req.body.data.like , bad: req.body.data.bad}}, function(err){
+    if(err) {
+      return console.log(err);
+    } else {
+      console.log(req)
+    }
+  })
+});
 
 var port = 8000;
-app.listen(port, function(){
+
+app.listen(port, function() {
   console.log('server on! http://localhost:'+port);
 });
